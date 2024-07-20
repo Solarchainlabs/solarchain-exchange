3eb8ef72cb8eb4e197c996160936f770ff9d7076
kakelyo <kakelyo@hotmail.com>
2024-07-20 10:52:06 +0800

    Update build.sh
    change network

--- projects/exchange-v2/build.sh
+++ projects/exchange-v2/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 truffle compile
-truffle migrate --f 2 --to 2 --network bsctest --compile-none
-truffle run verify SolarExchangeV2 --network bsctest
+truffle migrate --f 2 --to 2 --network ethtest --compile-none
+truffle run verify SolarExchangeV2 --network ethtest
